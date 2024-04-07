bc8c82ac34e75966f362a833fd3180f50b06893d
Amisha-0505 <amishashaw375@gmail.com>
2024-04-07 12:31:51 +0530

    complete the signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 
-export default function SignIn() {
+export default function SignIp() {
+  const [formData, setFormData] = useState({});
+  const [error,setError]=useState(false);
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try{
+      setLoading(true);
+      setError(false);
+      const response=await fetch("http://localhost:3000/api/auth/signin", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+      });
+    
+      const data=await response.json();
+      console.log(data);
+      setLoading(false);
+     if(data.success===false){
+      setError(true);
+      return;
+     }
+     navigate('/');
+    }catch(err){
+      setLoading(false);
+      setError(true);
+      // console.log(err);
+    }
+  }
+  //console.log(formData);
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center py-7 font-semibold'>Sign-In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input 
+        className='bg-slate-100 rounded-lg p-3' 
+        type='email' 
+        placeholder='Email' 
+        id='email'
+        onChange={handleChange}
+        />
+        <input 
+        className='bg-slate-100 rounded-lg p-3' 
+        type='password' 
+        placeholder='Password' 
+        id='password'
+        onChange={handleChange} 
+        />
+        <button 
+          disabled={loading}
+          className='uppercase bg-slate-800 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-70'>
+         {loading?'Loading....':'sign-in'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5 justify-center'>
+        <p className=''>Don't have an account? </p>
+        <Link to='/signup'>
+          <span className='text-sky-500'>sign-up</span>
+        </Link>
+      </div>
+      <p className='text-red-700 mt-5'>{error && 'something wend wrong!'}</p>
+    </div>
   )
 }
+
